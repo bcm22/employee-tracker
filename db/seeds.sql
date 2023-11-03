ffd02ae4efe2771e2134f004d92404b2a633c64e
bcm22 <135455152+bcm22@users.noreply.github.com>
2023-11-03 17:42:03 -0500

    Started to create seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO department(department_name)
+VALUES ("Engineering"),
+       ("Marketing"),
+       ("Accounting"),
+       ("Customer Service"),
+       ("Product Managemant");
+
+INSERT INTO role (title, salary, department_id, salary)
+VALUES ("Software Engineer I", 1, 75000),
+       ("Software Engineer II", 1, 125000),
+       ("IT Manager", 1, 100000),
+       ("Advertising Agent", 2, 63000),
+       ("Marketing Manager", 2, 90000),
+       ("Accounts Receivable Rep", 3, 45000),
+       ("Accounts Payable Rep", 3, 55000),
+       ("Account Manager", 3, 73000),
+       ("Customer Service Rep", 4, 45000),
+       ("Customer Service Manager", 4, 60000),
+       ("Product Support", 5, 55000),
+       ("Quality Assurance Rep", 5, 73000),
+       ("Quality Assurance Manager", 5, 102000);

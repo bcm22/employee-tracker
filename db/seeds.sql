cb2907d7c202b0951fb7f44a018039135a3a1d06
bcm22 <135455152+bcm22@users.noreply.github.com>
2023-11-04 13:17:48 -0500

    completed seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -19,3 +19,18 @@ VALUES ("Software Engineer I", 1, 75000),
        ("Product Support", 5, 55000),
        ("Quality Assurance Rep", 5, 73000),
        ("Quality Assurance Manager", 5, 102000);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ("ethan", "parker", 1, null),
+       ("olivia", "johnson", 1, null),
+       ("ben", "adams", 1, 1),
+       ("sophia", "martinez", 2, null),
+       ("jackson", "carter", 2, 2),
+       ("emma", "anderson", 3, null),
+       ("lucas", "thompson",3 , null),
+       ("mia", "davis", 3, 3),
+       ("aiden", "white", 4, null),
+       ("harper", "robinson", 4, 4),
+       ("mason", "clark", 5, null),
+       ("ava", "wilson", 5, null),
+       ("liam", "harris", 5, 5);

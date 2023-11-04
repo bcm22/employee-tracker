b5f300f6232b9dc1e461dd4bddacc8a21fe74170
bcm22 <135455152+bcm22@users.noreply.github.com>
2023-11-04 18:39:14 -0500

    fixed routing errors

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,15 @@
-DROP DATABASE IF EXISTS employees;
-CREATE DATABASE employees;
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
 
-USE employees;
+USE employees_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30)
 );
 
-CREAT TABLE role (
-    in INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     department_id INT,
     salary DECIMAL(8,2),

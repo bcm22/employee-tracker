081ed672e67abb394fa329716194b4ae93fd1cb2
bcm22 <135455152+bcm22@users.noreply.github.com>
2023-11-04 13:34:29 -0500

    created connection to db

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+require("dotenv").config();
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: process.env.DB_PASSWORD,
+        database: 'employees',
+    },
+    console.log("Connected to Employee Database")
+);
+
+module.exports = db;
